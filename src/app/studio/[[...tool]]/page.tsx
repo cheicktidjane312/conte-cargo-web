698e8e9b8ea07ac8c33fdc277ce0a6db33719582
Cheick Tidjane <cheicktidjane312@gmail.com>
2026-02-02 20:31:07 +0000

    Fix studio client side and structure

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,9 @@
+'use client'
+
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config' // 4 points = retour à la racine
+import config from '../../../../sanity.config'
 
-// On force le mode dynamique pour éviter que Vercel essaie de "deviner" la page
 export const dynamic = 'force-dynamic'
-
-// Plus besoin d'exporter metadata ici, c'est géré par le nouveau layout !
 
 export default function StudioPage() {
   return <NextStudio config={config} />
